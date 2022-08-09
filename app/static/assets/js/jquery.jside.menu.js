41974dddc19ee774422c44d493b6c794b7c30801
kenan0x0 <kenannakola@hotmail.com>
2022-08-09 09:17:38 +0000

    commit

--- app/static/assets/js/jquery.jside.menu.js
+++ app/static/assets/js/jquery.jside.menu.js
@@ -1,82 +1,91 @@
+/*!  Plugin: jSide Menu (Responsive Side Menu) 
+ *   Dependency: jQuery 3.4.1 & Material Design Iconic Font 2.0
+ *   Author: Asif Mughal
+ *   GitHub: https://github.com/CodeHimBlog
+ *   URL: https://www.codehim.com
+ *   License: MIT License
+ *   Copyright (c) 2018 - 2019 - Asif Mughal
+ */
+/* File: jquery.jside.menu.js */
 (function($){
-     $.fn.jSideMenu = function(options){
-    var setting = $.extend({
-   		   jSidePosition: "position-left", //possible options position-left or position-right 
-           jSideSticky: true, // menubar will be fixed on top, false to set static
-           jSideSkin: "default-skin", // to apply custom skin, just put its name in this string
-           
-		   jSideTransition: 400, //Define the transition duration in milliseconds 
-  
-  		   }, options);
-  
-  return this.each(function() {
-  var jSide, target, headHeight, devHeight, arrow, dimBackground;   
-   
-        target = $(this);
-		
+   $.fn.jSideMenu = function(options){
+  var setting = $.extend({
+          jSidePosition: "position-left", //possible options position-left or position-right 
+         jSideSticky: true, // menubar will be fixed on top, false to set static
+         jSideSkin: "default-skin", // to apply custom skin, just put its name in this string
+         
+       jSideTransition: 400, //Define the transition duration in milliseconds 
+
+         }, options);
+
+return this.each(function() {
+var jSide, target, headHeight, devHeight, arrow, dimBackground;   
+ 
+      target = $(this);
+    
 //Accessing DOM
-  jSide = $(".menu-container, .menu-head");
-  devHeight = $(window).height();
-  headHeight = $(".menu-head").height();
-  dHeading = $(".dropdown-heading");
-  menuTrigger = $(".menu-trigger");
-  arrow = $("<i></i>");
-  dimBackground = $("<div>");
-  
+jSide = $(".menu-container, .menu-head");
+devHeight = $(window).height();
+headHeight = $(".menu-head").height();
+dHeading = $(".dropdown-heading");
+menuTrigger = $(".menu-trigger");
+arrow = $("<i></i>");
+dimBackground = $("<div>");
+
 // Set the height of side menu according to the available height of device
 $(target).css({
-    'height' : devHeight-headHeight,
+  'height' : devHeight-headHeight,
 
 });
 
-    if (setting.jSideSticky == true){
-     $(".menubar").addClass("sticky");
+  if (setting.jSideSticky == true){
+   $(".menubar").addClass("sticky");
 }  else{
-     $(".menubar").removeClass("sticky");
-   }
+   $(".menubar").removeClass("sticky");
+ }
 
 $(".menubar").addClass(setting.jSideSkin);
- $(jSide).addClass(setting.jSideSkin).addClass(setting.jSidePosition);
+$(jSide).addClass(setting.jSideSkin).addClass(setting.jSidePosition);
 
-   if ($(jSide).hasClass("position-left")){
+ if ($(jSide).hasClass("position-left")){
 $(".menu-trigger").addClass("left").removeClass("right");
-     }
-  else{
-$(".menu-trigger").removeClass("left").addClass("right");
    }
+else{
+$(".menu-trigger").removeClass("left").addClass("right");
+ }
 
 //Dropdown Arrow
-    $(arrow).addClass("material-icons d-arrow").html("keyboard_arrow_down").appendTo(dHeading);
+  $(arrow).addClass("material-icons d-arrow").html("keyboard_arrow_down").appendTo(dHeading);
 //Dim Layer	
-    $(dimBackground).addClass("dim-overlay").appendTo("body");
+  $(dimBackground).addClass("dim-overlay").appendTo("body");
 //Dropdowns
 $(dHeading).click(function(){
-   $(this).parent().find("ul").slideToggle(setting.jSideTransition);
-   $(this).find(".d-arrow").toggleClass("d-down");
-    
- });
+ $(this).parent().find("ul").slideToggle(setting.jSideTransition);
+ $(this).find(".d-arrow").toggleClass("d-down");
+  
+});
 
 $(menuTrigger).click(function(){
-   $(jSide).toggleClass("open");
-   $(dimBackground).show(setting.jSideTransition);
-   $(".menu-body").removeClass("visibility");
+ $(jSide).toggleClass("open");
+ $(dimBackground).show(setting.jSideTransition);
+ $(".menu-body").removeClass("visibility");
 });
 
 //close menu if user click outside of it
-   $(window).click(function(e) {
-    if ($(e.target).closest('.menu-trigger').length){
-     return;}
-    if ($(e.target).closest(jSide).length){
-     return;}
-    
- $(jSide).removeClass("open");
-    if (!$(jSide).hasClass("open")) {
-     $(dimBackground).hide(setting.jSideTransition);
-        }
- $(".menu-body").addClass("visibility");
-      });
-   });
- };
+ $(window).click(function(e) {
+  if ($(e.target).closest('.menu-trigger').length){
+   return;}
+  if ($(e.target).closest(jSide).length){
+   return;}
+  
+$(jSide).removeClass("open");
+  if (!$(jSide).hasClass("open")) {
+   $(dimBackground).hide(setting.jSideTransition);
+      }
+$(".menu-body").addClass("visibility");
+    });
+ });
+};
 
 })(jQuery);
 /*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
