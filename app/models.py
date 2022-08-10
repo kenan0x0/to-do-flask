d2f2a2702fc16834a30a09b70e8f017fe0169350
kenan0x0 <kenannakola@hotmail.com>
2022-08-10 09:17:42 +0000

    fin models

--- app/models.py
+++ app/models.py
@@ -9,13 +9,18 @@ class Users(db.Model, UserMixin):
     password = db.Column(db.String(500))
     user_image = db.Column(db.String(), unique=False, nullable=True)
     creation_time = db.Column(db.Date, nullable=False, unique=False)
+    gender = db.Column(db.String(), unique=False, nullable=True, default=None)
+    full_name = db.Column(db.String(), unique=False, nullable=True, default=None)
+    city = db.Column(db.String(), unique=False, nullable=True, default=None)
 
     def __init__(self, user, email, password, user_image, creation_time):
         self.user = user
-        self.password = password
         self.email = email
+        self.password = password
         self.user_image = user_image
         self.creation_time = creation_time
+        
+
 
 class Tasks(db.Model, UserMixin):
     __tablename__ = 'Tasks'
